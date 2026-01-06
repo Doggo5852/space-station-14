11c38638e774c0010cb034a0f868a3bc60ee44c1
Doggo5852 <bombasticsideeye614@gmail.com>
2026-01-06 14:53:57 +0100

    IFF for missiles

---
+++ Content.Server/Mech/Components/MechIFFComponent.cs
@@ -0,0 +1,13 @@
+namespace Content.Server.Mech.Components;
+/// <summary>
+/// Marker for iff
+/// </summary>
+[RegisterComponent]
+public sealed partial class MechRaidersComponent : Component
+{
+}
+
+[RegisterComponent]
+public sealed partial class MechColonyComponent : Component
+{
+}
