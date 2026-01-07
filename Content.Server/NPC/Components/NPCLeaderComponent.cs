880805d1f866552b3bd5ed22aec29a48904d6eef
Doggo5852 <bombasticsideeye614@gmail.com>
2026-01-07 21:44:22 +0100

    NPC Leader

---
+++ Content.Server/NPC/Components/NPCLeaderComponent.cs
@@ -0,0 +1,9 @@
+namespace Content.Server.NPC.Components;
+
+/// <summary>
+/// Marker for an NPC leader
+/// </summary>
+[RegisterComponent]
+public sealed partial class NPCLeaderComponent : Component
+{
+}
